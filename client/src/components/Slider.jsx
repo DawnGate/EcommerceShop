2b18cc85f9e1c9b591ed7b7d42c8055a253a7363
DawnGate <lam99quyen@gmail.com>
2022-08-17 02:08:33 +0700

    update code 178

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -5,8 +5,8 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
 `
 
 const Arrow = styled.div.attrs((props) => ({
@@ -30,26 +30,50 @@ const Arrow = styled.div.attrs((props) => ({
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
 `
 
-const Slide = styled.div`
+const Slide = styled.div.attrs((props) => ({
+  bg: props.bg || ''
+}))`
   display: flex;
-  align-item: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #${(props) => props.bg};
 `
 
 const ImgContainer = styled.div`
+  height: 100%;
   flex: 1;
 `
 
-const Image = styled.img``
+const Image = styled.img`
+  height: 80%;
+`
 
 const InfoContainer = styled.div`
   flex: 1;
+  padding: 50px;
+`
+
+const Title = styled.h1`
+  font-size: 70px;
+`
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `
 
 const Slider = () => {
-  
-  
   return (
     <Container>
       <Arrow direction="left">
@@ -57,11 +81,45 @@ const Slider = () => {
       </Arrow>
 
       <Wrapper>
-    <ImgContainer>
-    <Image src="https://i.ibb.co/XsdmR2c/1.png" />
-    </ImgContainer>
-    
-        <InfoContainer></InfoContainer>
+        <Slide bg="f5fafd">
+          <ImgContainer>
+            <Image src="https://image.made-in-china.com/202f0j00HlwpujDFwJkZ/Hot-Selling-Online-Shopping-Summer-Holiday-Women-Tops.jpg" />
+          </ImgContainer>
+
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>
+              DON'T PROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fcf1ed">
+          <ImgContainer>
+            <Image src="https://image.made-in-china.com/202f0j00HlwpujDFwJkZ/Hot-Selling-Online-Shopping-Summer-Holiday-Women-Tops.jpg" />
+          </ImgContainer>
+
+          <InfoContainer bg="fbf0f4">
+            <Title>POPULAR SALE</Title>
+            <Desc>
+              DON'T PROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://image.made-in-china.com/202f0j00HlwpujDFwJkZ/Hot-Selling-Online-Shopping-Summer-Holiday-Women-Tops.jpg" />
+          </ImgContainer>
+
+          <InfoContainer>
+            <Title>WINTER SALE</Title>
+            <Desc>
+              DON'T PROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
       </Wrapper>
 
       <Arrow direction="right">
