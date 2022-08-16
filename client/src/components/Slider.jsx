038d799e831094667a08e5b006fe6c4b2fdd03df
Ubuntu-kr3k9n <lam99quyen@gmail.com>
2022-08-16 14:59:02 +0700

    update 16/8

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -5,23 +5,66 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+  background-color: coral;
+  position: relative;
 `
 
-const Arrow = styled.div`
+const Arrow = styled.div.attrs((props) => ({
+  direction: props.direction || ''
+}))`
   width: 50px;
   height: 50px;
   background-color: #fff7f7;
   border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === 'left' && '10px'};
+  right: ${(props) => props.direction === 'right' && '10px'};
+  margin: auto;
+  opacity: 0.5;
+`
+
+const Wrapper = styled.div`
+  height: 100%;
+`
+
+const Slide = styled.div`
+  display: flex;
+  align-item: center;
+`
+
+const ImgContainer = styled.div`
+  flex: 1;
+`
+
+const Image = styled.img``
+
+const InfoContainer = styled.div`
+  flex: 1;
 `
 
 const Slider = () => {
+  
+  
   return (
     <Container>
-      <Arrow>
+      <Arrow direction="left">
         <ArrowLeftOutlined />
       </Arrow>
 
-      <Arrow>
+      <Wrapper>
+    <ImgContainer>
+    <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+    </ImgContainer>
+    
+        <InfoContainer></InfoContainer>
+      </Wrapper>
+
+      <Arrow direction="right">
         <ArrowRightOutlined />
       </Arrow>
     </Container>
