092c9155ed918a9f9cf55b84bf5e1ad5983266af
Ubuntu-kr3k9n <lam99quyen@gmail.com>
2022-08-18 09:05:32 +0700

    add categories

--- client/src/components/Catergories.jsx
+++ client/src/components/Catergories.jsx
@@ -3,7 +3,11 @@ import styled from 'styled-components'
 import { categories } from '../data'
 import CategoryItem from './CatergoryItem'
 
-const Container = styled.div``
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+`
 
 const Category = () => {
   return (
